15c2ed4b36c8ca068cb80701d02926629771526f
rajat singh <rajatsingh997.rs@gmail.com>
2019-12-26 15:42:43 +0530

    firstcommit

---
+++ src/main/java/com/firstdemo/first_demo/FirstDemoApplication.java
@@ -0,0 +1,15 @@
+package com.firstdemo.first_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstDemoApplication.class, args);
+
+    System.out.println("welcome");
+    }
+
+}
